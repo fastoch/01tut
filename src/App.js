cdc5474c6a157e8d2e5c79d3c6b423140c59cc72
fastoch <fastoch@ik.me>
2023-12-30 21:29:36 +0100

    @30min

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   const handleNameChange = () => {
@@ -10,12 +10,7 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-      </header>
+      <Header />
     </div>
   );
 }
