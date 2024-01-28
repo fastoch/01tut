933156c1a55f3df07c030eb1425bd38800afb679
fastoch <fastoch@ik.me>
2024-01-28 14:55:16 +0100

    @92min

--- src/Content.js
+++ src/Content.js
@@ -21,16 +21,23 @@ const Content = () => {
     }
   ])
 
-  // Identify the checked item by its id & Reverse the state of its 'checked' property 
+  // create new list by reversing the 'checked' property of checked item
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
     setItems(listItems)
     localStorage.setItem('shoppingList', JSON.stringify(listItems))
   }
 
+  // create new list by filtering out the item that is passed in to the func
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id)
+    setItems(listItems)
+    localStorage.setItem('shoppingList', JSON.stringify(listItems))
+  }
+
   return (
     <main>
-      <ul role="list">
+      <ul>
         {items.map((item) => (
           <li className="item" key={item.id}>
             <input 
@@ -43,6 +50,7 @@ const Content = () => {
               style={(item.checked) ? { textDecoration: 'line-through'} : null}
             >{item.item}</label>
             <FaTrashAlt 
+              onClick={() => handleDelete(item.id)}
               role="button" 
               tabIndex="0" 
             />
